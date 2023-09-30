8d99bfc2d0088044733ff349d0c76b49754a57f5
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-09-30 11:47:22 -0600

    all 03 tests passing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let arrayToReverse = stringToReverse.split('');
+    let reversedArray = [];
+    for (let i = 0; i < stringToReverse.length; i++) {
+        reversedArray[i] = arrayToReverse[arrayToReverse.length - 1 - i];
+    }
+    let reversedString = reversedArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
