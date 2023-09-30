7b3803b684ea9297ee4ff16d2f3a467cb001a976
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-09-30 16:16:14 -0600

    passed first test for 04 on first try :)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, toRemove) {
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] == toRemove) {
+            inputArray.splice(i, 1);
+            return inputArray;
+        }
+    }
 };
 
 // Do not edit below this line
