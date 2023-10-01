914a45375bb0b93759f9c0c35697d8df7a022d77
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-01 13:31:41 -0600

    passes first and second tests for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,13 @@
 const removeFromArray = function(inputArray, ...toRemove) {
-    console.log(toRemove);
-    for (let i = 0; i < inputArray.length; i++) {
-        for (let j = 0; j < toRemove.length; j++) {
-            if (inputArray[i] == toRemove[j]) {
-                inputArray.splice(i, 1);
-                console.log(inputArray);
-            }
+    let resultArray = [];
+    inputArray.forEach((item) => {
+        if (!toRemove.includes(item)) {
+            resultArray.push(item);
         }
-    }
-    return inputArray;
+    });
+    return resultArray;
 }
 
-removeFromArray([1, 2, 3, 4, 5], 3, 2);
+//removeFromArray([1, 2, 3, 4, 5], 3, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
