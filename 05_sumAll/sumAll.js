ecc8b2f0cf3cf663408a3cb471c625131a6cf09c
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-01 13:46:33 -0600

    passes all 05 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    let sum = 0;
+    if (numA < 0 || numB < 0 || typeof(numA) != 'number' || typeof(numB) != 'number') {
+        return "ERROR";
+    }
 
+    if (numA > numB) {
+        for (let i = numB; i <= numA; i++) {
+            sum += i;
+        }
+    } else {    
+        for (let i = numA; i <= numB; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
