0aa3f2043cb74c796c5984224e77d2d97e6e45f8
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-10-01 15:13:57 -0600

    passes all 07 tests -- first try!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(initialTemp) {
+    let finalTemp = Math.round(((initialTemp - 32) * (5/9)) * 10) / 10;
+    return finalTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(initialTemp) {
+    let finalTemp = Math.round((initialTemp * (9/5) + 32) * 10) / 10;
+    return finalTemp;
 };
 
 // Do not edit below this line
